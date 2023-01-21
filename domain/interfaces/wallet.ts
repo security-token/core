1ec412d82a7daead260d4eeb522845d71ef3b5bf
Amaury Diaz <amauryuh@gmail.com>
2023-01-21 16:10:23 +0100

    correct mistake

--- domain/interfaces/wallet.ts
+++ domain/interfaces/wallet.ts
@@ -7,7 +7,7 @@ type ProjectData = [SecurityToken.Metadata, boolean];
 
 export interface IWallet {
   getWallet(): Promise<Wallet>;
-  setWallet(): Promise<Wallet>;
+  setWallet(wallet: Wallet): Promise<boolean>;
 
   getCertifiedIssuer(address: string): Promise<IssuerData>;
   getCertifiedProject(address: string): Promise<ProjectData>;
