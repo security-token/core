f0c5888030ec69b925baa94df0e4f8a955824528
Amaury Diaz <amauryuh@gmail.com>
2023-01-21 16:34:08 +0100

    change in wallet interface

--- domain/interfaces/wallet.ts
+++ domain/interfaces/wallet.ts
@@ -2,13 +2,10 @@ import { SecurityToken, Issuer, Document, Partition } from "../schema/token_pb";
 import { Account } from "../schema/account_pb";
 import { Wallet } from "../schema/wallet_pb";
 
-type IssuerData = [Issuer.Metadata, boolean];
-type ProjectData = [SecurityToken.Metadata, boolean];
-
 export interface IWallet {
   getWallet(): Promise<Wallet>;
   setWallet(wallet: Wallet): Promise<boolean>;
 
-  getCertifiedIssuer(address: string): Promise<IssuerData>;
-  getCertifiedProject(address: string): Promise<ProjectData>;
+  getCertifiedIssuer(address: string): Promise<Issuer.Metadata | null>;
+  getCertifiedProject(address: string): Promise<SecurityToken.Metadata | null>;
 }
