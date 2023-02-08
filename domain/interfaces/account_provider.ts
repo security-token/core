28b1636999fc808c3326d3f38d5c9e7a30961d1d
Amaury Diaz <amauryuh@gmail.com>
2023-02-08 10:13:24 -0500

    release v0.0.53

--- domain/interfaces/account_provider.ts
+++ domain/interfaces/account_provider.ts
@@ -4,6 +4,10 @@ export interface IAccountProvider {
   getAddress(): Promise<string>;
   getAmount(): Promise<BigInt>;
 
+  connected(): Promise<boolean>;
+  connect(): Promise<boolean>;
+  disconnect(): Promise<boolean>;
+
   signMessage(message: string): Promise<string>;
   sendTransaction<T>(tx: Transaction): Promise<T>;
 }
